02e8847ccfbc50c07c2eff2654f5358ea678f26a
ShittyRobot <01001010kevin@gmail.com>
2018-08-29 23:06:45 -0400

    ready to submit

--- 2018FallProblems/HW03/josephus.c
+++ 2018FallProblems/HW03/josephus.c
@@ -20,10 +20,45 @@ void eliminate(int n, int k)
       return;
     }
   // initialize all elements
-
-
-  
+    for(int j=0; j<n; j++){
+        arr[j] = -1;
+    }
   // counting to k,
+
+    int counter = 1;
+    int done = 0;
+    int numPlacement = 0;
+    int arrayIndex = 0;
+   
+    while(!done){
+        if(arrayIndex == n){
+            arrayIndex = 0;
+        }
+        if(counter == k + 1){
+            counter = 1;
+        }
+        if(numPlacement == n){
+            done = 1;
+        }else{
+            if(counter == k){
+                if(arr[arrayIndex] != 0){
+                    printf("%d\n",arrayIndex);
+                    arr[arrayIndex] = 0;
+                    numPlacement ++;
+                    counter++;
+                }
+            }else{
+                if(arr[arrayIndex] != 0){
+                    arr[arrayIndex] = counter;
+                    counter++;
+                }
+            }
+            arrayIndex++;
+            
+        }
+    
+    }
+   
   // mark the eliminated element
   // print the index of the marked element
   // repeat until only one element is unmarked
