3b2bd8a3f4f6787cd7e8d5ba07d863fa7dabedbf
ShittyRobot <01001010kevin@gmail.com>
2018-11-04 12:37:30 -0500

    intial commit

---
+++ 2018FallProblems/HW15/hw15.c
@@ -0,0 +1,124 @@
+/****************** Modify this file at specified place *************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include "hw15.h"
+
+#ifdef LINKEDLIST
+// Do not modify this function.
+void LinkedListPrint(Node * head)
+{
+
+	Node *temp = head;
+
+	while(temp!=NULL)
+	{
+		if(temp->next!=NULL)
+		{
+			printf("%d,",temp->value);
+		}
+		else
+		{
+			printf("%d\n",temp->value);
+		}
+		temp=temp->next;
+	}
+}
+#endif
+
+
+/********** Do Not modify the file above this line, you can modify below ***********/
+#ifdef TEST_CREATENODE
+
+Node * CreateNode(int val)
+{
+    // same as previous homeworks
+    Node * newNode = malloc(sizeof(Node));
+    
+    // check memory allocation fails
+    if (newNode == NULL) {
+        return NULL;
+    }
+    // create a Node* with the value as 'value'(input argument).
+    newNode -> value = val;
+    newNode -> next = NULL;
+    return newNode;
+}
+#endif
+
+#ifdef TEST_LINKEDLISTCREATE
+
+void LinkedListCreate(Node ** head, char* name)
+{
+	/*
+	Create a linked list from the input file referenced by `name`.
+	*/
+    FILE * fp = fopen(name, "r");
+    int len = 0;
+    if (fp == NULL) {
+        printf("error while attemp to open file");
+        return EXIT_FAILURE;
+    }
+    
+    //count the number of numbers in file
+    int waste = 0;
+    while (fscanf(fp, "%d", &waste) == 1)len++;
+    
+    //malloc array
+    int * arr = malloc(sizeof(int)*len);
+
+    if (arr == NULL) {
+        fclose(fp);
+        return EXIT_FAILURE;
+    }
+    
+    //reset fp
+    fseek(fp,0,SEEK_SET);
+
+    int ind = 0;
+    while (ind < len)
+    {
+        if (fscanf(fp, "%d", &arr[ind]) != 1)
+        {
+            fprintf(stderr, "fscanf fail\n");
+            fclose (fp);
+            free (arr);
+            return EXIT_FAILURE;
+        }
+        ind ++;
+    }
+    
+    
+    if (len <= 0) {
+        * head = NULL;
+        fclose(fp);
+        free(arr);
+        return;
+    }else{
+        * head = CreateNode(name[0]); //create the head of the node
+        Node * temp = * head;
+        for (int i = 1; i < len; i++) {
+            temp -> next = CreateNode(arr[i]);
+            temp = temp -> next;
+        }
+        fclose(fp);
+        free(arr);
+        return;
+    }
+    
+}
+#endif
+#ifdef TEST_REMOVED
+//This function will remove repetitions of a linked list value.
+
+void RemoveDuplicate(Node *headRef)
+{
+	/*
+	This function will remove repetitions from the linked list referenced by headRef.
+	Each time we check for a new Node in the linked list, we will check if it is distinct from
+	the Nodes already present in the list ( upto the previous Node).
+	So for each occurrence after its first, we will be deleting that node.
+	To delete a node: we will map the next Node of the previous Node to the Node after the current Node.
+	Print the linked list after all repetitions have been removed. Use the function given to you for priniting.
+	*/
+}
+#endif
---
+++ 2018FallProblems/HW15/main.c
@@ -0,0 +1,31 @@
+/****************** Modify this file at specified place *************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hw15.h"
+
+/****************** DO NOT Modify this file above this line *************************/
+
+// Modify the following function
+#ifdef TEST_MAIN
+
+int main(int argc, char **argv)
+{
+	// input argv[1] contains the name of the input file.
+	// Check for the right number of input arguments.
+    if (argc<2) {
+        return EXIT_FAILURE;
+    }
+    
+    Node * head;
+    LinkedListCreate(&head,argv[1]);
+    
+    LinkedListPrint(head);
+    
+	//Initialize a linked list Node and use the file name from input arguments to create a linked list
+	//Remove all duplicates in the linked list created above.
+
+	// release memory
+}
+
+#endif
