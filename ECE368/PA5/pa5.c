417fc47b1380fdc63e464db820c23e4d3f70d64a
ShittyRobot <01001010kevin@gmail.com>
2019-04-05 12:36:06 -0400

    init

---
+++ ECE368/PA5/pa5.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "sorting.h"
+
+long * Load_Into_Array(char * Filename, int * Size);
+int Save_From_Array(char * Filename, long * Array, int Size);
+
+int main(int argc, char ** argv)
+{
+  // argv[1]: mode -q for quicksort, -m for mergesort
+  // argv[2]: inputName
+  // argv[3]: outputName
+  long * inputArr = NULL; //input array reading from file
+  int sizeofInputArr = 0;
+
+  if (argc != 4) {
+    printf("Wrong Argument Count (usage: 3 Argument)\n");
+    return EXIT_FAILURE;
+  }
+
+  inputArr = Load_Into_Array(argv[2],&sizeofInputArr);
+
+  if (strcmp(argv[1],"-q") == 0) {
+    Quick_Sort(inputArr,sizeofInputArr);
+    free(inputArr);
+    return EXIT_SUCCESS;
+  }else if (strcmp(argv[1],"-m") == 0) {
+    Merge_sort(inputArr,sizeofInputArr){
+    free(inputArr);
+    return EXIT_SUCCESS;
+  }
+  printf("Wrong Argument (usage: -l or -a)\n");
+  return EXIT_FAILURE;
+
+}
+
+long * Load_Into_Array(char * Filename, int * Size){
+  *Size = 0;
+  FILE * inputFilePtr = fopen(Filename,"r");
+  long curLong = 0;
+  long * numArray = NULL;
+
+  if(inputFilePtr == NULL){
+    printf("Input File Does Not Exist\n");
+    fclose(inputFilePtr);
+    return NULL;
+  }
+
+  while(fread(&curLong, sizeof(long), 1, inputFilePtr) == 1) (*size)++;
+  fseek(inputFilePtr, 0, SEEK_SET); //now put back the file pointer
+  numArray = malloc(sizeof(long)*(*size));
+
+  if (numArray == NULL) {
+    printf("Faild to Malloc");
+    fclose(inputFilePtr);
+    return NULL;
+  }
+
+  for(int i = 0 ; i < *size ; i++){
+    fread(&curLong, sizeof(long), 1, inputFilePtr);
+    numArray[i] = curLong;
+    // printf("numArray[%d]:%ld\n",i,curLong);
+  }
+  fclose(inputFilePtr);
+  return numArray;
+}
+
+int Save_From_Array(char * Filename, long * Array, int Size){
+  long trueSize = 0;
+  FILE * outputFilePtr = fopen(Filename,"w");
+  if(outputFilePtr == NULL){
+    printf("Can not create file\n");
+    fclose(outputFilePtr);
+    return 0;
+  }
+  if (Size == 0) return 0; //create empty file for zero byte array
+  trueSize = fwrite(Array,sizeof(long),Size,outputFilePtr);
+  fclose(outputFilePtr);
+
+  return trueSize;
+}
---
+++ ECE368/PA5/sorting.c
@@ -0,0 +1,8 @@
+#include "sorting.h"
+
+void Quick_Sort(long * Array, int Size){
+
+}
+void Merge_sort(long * Array, int Size){
+  
+}
