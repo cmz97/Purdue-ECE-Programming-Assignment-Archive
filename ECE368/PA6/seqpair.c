0e178927fea948b13c03c2fa9c390712956acc68
ShittyRobot <01001010kevin@gmail.com>
2019-04-26 07:59:23 -0400

    check valgrind again

--- ECE368/PA6/seqpair.c
+++ ECE368/PA6/seqpair.c
@@ -2,7 +2,8 @@
 
 
 int * generateLUT(int * seqOne, int sizeofSeq){
-  int * lut = malloc(sizeof(int)*(sizeofSeq + 1)); //account for the offsett to plus one
+  int * lut = NULL;
+  lut = malloc(sizeof(int)*(sizeofSeq + 1)); //account for the offsett to plus one
   for (int i = 0; i < sizeofSeq; i++) {
     lut[seqOne[i]] = i;
   }
